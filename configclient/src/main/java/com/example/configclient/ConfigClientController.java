2ab6759b39087e4445767dab4d55902aabb94fcf
Ryan Dawson <ryan.dawson@alfresco.com>
2018-03-23 09:08:02 +0000

    client and server projects

---
+++ configclient/src/main/java/com/example/configclient/ConfigClientController.java
@@ -0,0 +1,19 @@
+package com.example.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${message}")
+    private String message;
+
+    @GetMapping
+    public String hello(){
+        return "Hello "+message;
+    }
+}
